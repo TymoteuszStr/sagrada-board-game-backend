85e6a003b622addce0ec6e3dba0d80ba608e2989
Tymek <tymoteusz.strozniak@gmail.com>
2022-07-09 20:30:11 +0200

    add plater class

---
+++ backend/src/logic/game/player.ts
@@ -0,0 +1,48 @@
+import PatternCard from "./patternCard"
+import { IndividualTargetEnum } from '../helpers/individualTargetEnum';
+
+
+export default class Player {
+
+  public constructor(id: string, name: string) {
+    this.id = id
+    this.name = name
+  }
+  readonly id: string
+  readonly name: string
+  private _patternCard: PatternCard | undefined
+  private _individualTarget: IndividualTargetEnum | undefined
+  private _usedFavorTokens: number = 0
+  points: number = 0
+
+  get usedFavorTokens(): number {
+    return this._usedFavorTokens
+  }
+
+
+  get individualTarget(): IndividualTargetEnum | undefined {
+    return this._individualTarget
+  }
+  set individualTarget(arg: IndividualTargetEnum | undefined) {
+    this._individualTarget = arg
+  }
+
+  get patternCard(): PatternCard | undefined {
+    return this._patternCard
+  }
+  set patternCard(arg: PatternCard | undefined) {
+    this._patternCard = arg
+  }
+
+
+  useFavorToken(arg: number): void {
+    if (this.canUseFavorTokens(arg))
+      this._usedFavorTokens += arg
+  }
+
+  canUseFavorTokens(arg: number): boolean {
+    if (this._patternCard && this._usedFavorTokens + arg > this._patternCard.favorTokens) return false
+    else return true
+  }
+
+}
