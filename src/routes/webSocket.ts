7a37d3f25d109234685a8174033a63cd83271f20
Tymek <tymoteusz.strozniak@gmail.com>
2022-12-17 12:29:29 +0100

    add begginer chat messages functionaly

--- src/routes/webSocket.ts
+++ src/routes/webSocket.ts
@@ -3,11 +3,16 @@ import { Socket } from "socket.io";
 import RoomService from "../services/Game/RoomService";
 
 io.on('connection', (socket: Socket) => {
-    console.log('a user connected', socket.id);
-    socket.send(JSON.stringify({
-    }))
-    io.emit('playersInThisRoom',"test")
-    
-});
-// const listOfUsers= RoomServices.getUsersInRoom(id)
+    console.log('SOCKET ID', socket.id);
 
+    socket.on('enterToRoom', (id:string) => {
+        console.log('roomtId',id)
+        const listOfUsers = RoomService.getUsersInRoom(id)
+        socket.emit('playersInThisRoom',listOfUsers)
+    })
+
+    socket.on('sendMessage', (message:string) => {
+        socket.broadcast.emit('newMessage',message)
+    })
+    
+}); 
