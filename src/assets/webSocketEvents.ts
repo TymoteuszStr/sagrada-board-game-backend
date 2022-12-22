0782d313bf8e9b7ec51e265ce33460f1ac46cc3c
Tymek <tymoteusz.strozniak@gmail.com>
2022-12-22 19:45:27 +0100

    add few more web sockter events

---
+++ src/assets/webSocketEvents.ts
@@ -0,0 +1,4 @@
+export const USER_ENTER_ROOM = "USER_ENTER_TO_ROOM";
+export const USER_LEAVE_ROOM = "USER_ENTER_TO_ROOM";
+export const PLAYERS_LIST_IN_ROOM = "PLAYERS_LIST_IN_ROOM";
+export const NEW_MESSAGE = "NEW_MESSAGE";
--- src/routes/webSocket.ts
+++ src/routes/webSocket.ts
@@ -1,18 +1,30 @@
 import { io } from "../app";
 import { Socket } from "socket.io";
 import RoomService from "../services/Game/RoomService";
+import { NEW_MESSAGE, PLAYERS_LIST_IN_ROOM, USER_ENTER_ROOM, USER_LEAVE_ROOM } from "../assets/webSocketEvents";
 
 io.on('connection', (socket: Socket) => {
     console.log('SOCKET ID', socket.id);
 
-    socket.on('enterToRoom', (id:string) => {
-        console.log('roomtId',id)
-        const listOfUsers = RoomService.getUsersInRoom(id)
-        socket.emit('playersInThisRoom',listOfUsers)
+    socket.on(USER_ENTER_ROOM, (roomId:string, userId) => {
+        socket.join(roomId)
+        // RoomService.addPlayerToRoom(roomId,userId)
+        // const listOfUsers = RoomService.getUsersInRoom(roomId)
+        // console.log(listOfUsers)
+        // socket.emit(PLAYERS_LIST_IN_ROOM,listOfUsers)
+    })
+    socket.on(USER_LEAVE_ROOM, (roomId:string, userId) => {
+        socket.join(roomId)
+        // RoomService.removePlayerFromRoom(roomId,userId)
+        // const listOfUsers = RoomService.getUsersInRoom(roomId)
+        // console.log(listOfUsers)
+        // socket.emit(PLAYERS_LIST_IN_ROOM,listOfUsers)
     })
 
-    socket.on('sendMessage', (message:string) => {
-        socket.broadcast.emit('newMessage',message)
+    socket.on(NEW_MESSAGE, (text:string,roomId:string) => {
+        console.log(roomId);
+        const message = {date: new Date(), text,}
+        socket.broadcast.to(roomId).emit(NEW_MESSAGE,message)
     })
     
 }); 
